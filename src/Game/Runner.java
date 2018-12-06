174f6b5608229ac86709790c67becb3cbf27a51e
William <wleung3409@bths.edu>
2018-12-05 20:25:00 -0500

    room added!

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -2,6 +2,7 @@ package Game;
 
 import People.Person;
 import Rooms.Room;
+import Rooms.UnluckyRoom;
 import Rooms.WinningRoom;
 	
 import java.util.Scanner;
@@ -29,7 +30,10 @@ public class Runner {
 		int x = (int)(Math.random()*building.length);
 		int y = (int)(Math.random()*building.length);
 		building[x][y] = new WinningRoom(x, y);
-		 
+		int z = (int)(Math.random()*building.length);
+		int i = (int)(Math.random()*building.length);
+		building[z][i] = new UnluckyRoom(z,i);
+
 		 //Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
 		building[0][0].enterRoom(player1);
